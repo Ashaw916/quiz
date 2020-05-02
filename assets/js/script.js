13e215116e080520741a69849229b1ad988b05cc
ashaw916 <a.s.young530@gmail.com>
2020-05-02 01:18:20 -0700

    re worked script, fixed bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,131 +1,201 @@
-//array of question objects
-
-var $questions = [
-    {
-        question: "sgdfdfsdgssjfjshfg",
-        answers: [" ", " ", " ", " "],
-        get correctAnswer() {
-            return this.answers[1];
-        }
+//question object
+var $questArray = [
+    { 
+        question: "3?",
+        optionA: "1 ",
+        optionB: "2 ",
+        optionC: "3 ",
+        answer: "3 ",
     },
     {
-        question: "sgdfdfsdgssjfjshfg",
-        answers: [" ", " ", " ", " "],
-        get correctAnswer() {
-            return this.answers[1];
-        }
+        question: "1?",
+        optionA: "1 ",
+        optionB: "2 ",
+        optionC: "3 ",
+        answer: "1 ",
     },
     {
-        question: "sgdfdfsdgssjfjshfg",
-        answers: [" ", " ", " ", " "],
-        get correctAnswer() {
-            return this.answers[1];
-        }
+        question: "2?",
+        optionA: "1 ",
+        optionB: "2 ",
+        optionC: "3 ",
+        answer: "2 ",
     },
     {
-        question: "sgdfdfsdgssjfjshfg",
-        answers: [" ", " ", " ", " "],
-        get correctAnswer() {
-            return this.answers[1];
-        }
+        question: "3?",
+        optionA: "1 ",
+        optionB: "2 ",
+        optionC: "3 ",
+        answer: "3 ",
     },
     {
-        question: "sgdfdfsdgssjfjshfg",
-        answers: [" ", " ", " ", " "],
-        get correctAnswer() {
-            return this.answers[1];
-        }
-    }
+        question: "2?",
+        optionA: "1 ",
+        optionB: "2 ",
+        optionC: "3 ",
+        answer: "2 ",
+    },
+    {  
+        question: " ",
+        optionA: " ",
+        optionB: " ",
+        optionC: " ",
+        answer: " ",
+    },
 ];
 
-var $qIndex = 0;
-var $currentQuestion = $questions[$qIndex].question;
-    
 // variables to track score
 var $score = 0;
-
 //variable for place in test
 var $testLocation = 0;
 var $a1 = $("#a1");
 var $a2 = $("#a2");
 var $a3 = $("#a3");
 var $buttons = [$a1, $a2, $a3];
-
-//locations in html
-//  "test-sections"
-//  "start-btn"
-//  "timer"
-//  "question"
-//  "answers"
-//  "a1"
-//  "a2"
-//  "a3"
-//  "confirm"
-//  "answer-text"
-//  "next-btn"
-
 // timer
-var $totalSeconds = 120;
-var $secondsElapsed = 0;
-var $secondsLeft = 120;
+var $secondsLeft = 60;
 var $interval;
-
 //event listener for document
 $(document).ready(function() {
 
+    // event listener for start button
     $("#start-btn").on("click", function() {
+        // hide start after click
+        $("#start-btn").addClass("hide");
+        //show everything after click
+        $("#timer-div").removeClass("hide");
+        $("#question-div").removeClass("hide");
+        $("#answers").removeClass("hide");
+        $("#confirm").removeClass("hide");
+        $("#next-div").removeClass("hide");
+        // call function to populate question and answers
+        createQuestions();
 
-    $("#start-btn").attr("class", "hide")
-
-        function setTime() {
+        // timer function
+        function setTimer() {
+        
             var $timerInterval = setInterval(function() {
-              $secondsLeft--;
-              $("#timer").text($secondsLeft + "seconds left.");
-          
-              if($secondsLeft === 0) {
+                $secondsLeft--;
+                $("#timer").text($secondsLeft + " seconds left.");
+                if($secondsLeft === 0) {
                 clearInterval($timerInterval);
-                alert($score + " out of 4")
-              }
-            }, 1000);
-          }
+                alert($score + " out of 4");
+            }}, 
+                1000);
+            };
 
-          function sendMessage() {
-            $("#timer").text(" ");
-          }
-          
-          setTime();
+            function sendMessage() {   
+                $("#timer").text(" ");
+                $("#timer").removerClass("hide");
+            };
+            
+            setTimer();
     });
 
+    // function to populate question and answers
+    function createQuestions() {
+        $("#question").text($questArray[$testLocation].question);
+        $("#a1").text($questArray[$testLocation].optionA);
+        $("#a2").text($questArray[$testLocation].optionB);
+        $("#a3").text($questArray[$testLocation].optionC);
+    };
 
-    function makeQuiz() {
-
-        $("#question").text($currentQ);
-
-        for (i = 0; i < $buttons.length; i++) {
-            var $addAnswers = $questionObj[$qIndex].answers;
-            $buttons[i].text($addAnswers[i]);
+    //event listener for a1 button
+    $("#a1").on("click", function(){
+        // disable buttons
+        
+        if (($questArray[$testLocation].optionA) === ($questArray[$testLocation].answer)) {
+            console.log("correct");
+            // change color to green if correct and change font size to 2rem
+            $("#confirm").css({"color": "green", "font-size": "2rem"});
+            $score ++;
         }
-    }
-    //event listener for each answer button
-    
 
-        //if else right answer
-        function selectAnswer() {
+        else {
+            console.log("wrong");
+            // change color to red if wrong and change font size to 2rem
+            $("#confirm").css({"color": "red", "font-size": "2rem"});
+            $secondsLeft -10;
+        };
 
+        $("#confirm").text($questArray[$testLocation].answer);
+        console.log($score);
+        console.log($testLocation);
+    });
+
+    //event listener for a2 button
+    $("#a2").on("click", function(){
+        // disable buttons
+
+        if (($questArray[$testLocation].optionB) === ($questArray[$testLocation].answer)) {
+            console.log("correct");
+            // change color to green if correct and change font size to 2rem
+            $("#confirm").css({"color": "green", "font-size": "2rem"});
+            $score ++;
         }
 
-            // display answer 
-            //change answer text color depending on right/wrong
-            //change score
-            //change question number
+        else {
+            console.log("wrong");
+            // change color to red if wrong and change font size to 2rem
+            $("#confirm").css({"color": "red", "font-size": "2rem"});
+            $secondsLeft -10;
+        }
+
+        $("#confirm").text($questArray[$testLocation].answer);
+        console.log($score);
+        console.log($testLocation);
+    });
+
+    //event listener for a3 button
+    $("#a3").on("click", function(){
+        // disable buttons
+
+        if (($questArray[$testLocation].optionC) === ($questArray[$testLocation].answer)) {
+            console.log("correct");
+            // change color to green if correct and change font size to 2rem
+            $("#confirm").css({"color": "green", "font-size": "2rem"});
+            $score ++;
+        }
+
+        else {
+            console.log("wrong");
+            // change color to red if wrong and change font size to 2rem
+            $("#confirm").css({"color": "red", "font-size": "2rem"});
+            $secondsLeft --;
+        }
+        // show correct answer
+        $("#confirm").text($questArray[$testLocation].answer)
+        console.log($score);
+        console.log($testLocation);
+    });
 
     //event listener for next button
-    
-        //function to display next question object
-        function nextQuestion() {
+    $("#next-btn").on("click", function() {
+        // add one to test location
+        $testLocation++;
+        // call function to create quiz
+        createQuestions();
+        //call function to end
+        $endQuiz();
+    });
 
+    // end quiz,  show score, and hide everthing else
+    function $endQuiz() {
+        if ($testLocation === 4){
+            $("#next-btn").text("Done");
         }
-        //change button to say done on last question and display score
-
-
+        if ($testLocation === 5){
+            $("#confirm").text($score + " out of 5")
+            $("#timer").addClass("hide");
+            var initials = prompt("You Scored " + $score + " out of 5 " + "Enter Initials");
+            localStorage.setItem("initials", initials);
+        };
+        if ($secondsLeft === 0) {
+            $("#confirm").text($score + " out of 5")
+            $("#timer").addClass("hide");
+            var initials = prompt("You Scored " + $score + " out of 5 " + "Enter Initials");
+            var $scoreSave = $score;
+            localStorage.setItem("initials", initials);
+        };
+    };  
 });
